8e0585f05b5b46cbdc2bac774244712127eed6ba
Temesgen Birechis <birechis@gmail.com>
2025-12-17 14:11:48 +0300

    Edited Header.

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -11,7 +11,7 @@ export default async function HomeLayout({
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1 flex flex-col">{children}</main>
-      <Footer></Footer>
+      <Footer />
     </div>
   );
 }
