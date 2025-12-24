a27257cb59c295fd58422aedbd71d681c45b5062
Temesgen Birechis <birechis@gmail.com>
2025-12-24 12:05:01 +0300

    Connected to MongoDB.

---
+++ types/index.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+import { ProductInputSchema } from '@/lib/validator';
+
+export type IProductInput = z.infer<typeof ProductInputSchema>;
+
+export type Data = {
+  products: IProductInput[];
+  headerMenu: {
+    name: string;
+    href: string;
+  }[];
+  carousel: {
+    image: string;
+    title: string;
+    url: string;
+    buttonCaption: string;
+    isPublished: boolean;
+  }[];
+};
