747a02719cb7766d48e1844b9e44b5a3f5527fb5
Temesgen Birechis <birechis@gmail.com>
2026-02-18 17:46:33 +0300

    Edited constats value

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-//constants
+//constants used in amaclone
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'amaclone';
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
